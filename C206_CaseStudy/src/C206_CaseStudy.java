415345993f150d1afa1aa5ca1ab3616e830b604c
zhihelam <108645653+zhihelam@users.noreply.github.com>
2022-08-10 23:31:05 +0800

    Update C206_CaseStudy.java

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,10 +3,9 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("S");
 
 
-		System.out.println("Hi");
+		
 	}
 
 }
